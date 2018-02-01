7a27add96477009682e5f90356624b3ec8f504a2
JustAhmed <justahmed96@outlook.com>
2018-02-01 04:01:14 +0200

    1. Fixed some redirection errors. 2. Users can choose their tasks and mark it as done 3. Scrum master can edit tasks info. 4. Number of tasks for each product is fixed

--- ScrumManagementSystem/settings.py
+++ ScrumManagementSystem/settings.py
@@ -56,6 +56,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+            'libraries':{
+                'backlog_extras': 'backlog.templatetags.backlog_extras',
+            }
         },
     },
 ]
